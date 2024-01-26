c29a66a9bb8dba5a2b454d09a1e9ca1caaef4d32
Abid <abidroid1@gmail.com>
2024-01-26 16:59:46 +0500

    Form and validations

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -10,13 +10,89 @@ class ThirdScreen extends StatefulWidget {
 }
 
 class _ThirdScreenState extends State<ThirdScreen> {
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  late int num1, num2;
+  int result = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:  Text('Third Screen ${widget.name}'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: ListView(children: [
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'First Number',
+                border: OutlineInputBorder(),
+              ),
+              validator: (text){
+                if( text == null || text.isEmpty){
+                  return 'Please provide First Number';
+                }
+
+                if( text.length < 2 ){
+                  return 'Please provide a number with 2 or more digits';
+                }
+
+                // when text is valid
+
+                num1 = int.parse(text);
+                return null;
+              },
+            ),
+            const SizedBox(height: 16,),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText: 'Second Number',
+                border: OutlineInputBorder(),
+              ),
+              validator: (text){
+                if( text == null || text.isEmpty){
+                  return 'Please provide Second number';
+                }
+
+                if( text.length < 2 ){
+                  return 'Please provide a number with 2 or more digits';
+                }
+
+                num2 = int.parse(text);
+                return null;
+              },
+            ),
+            const SizedBox(height: 16,),
+
+            ElevatedButton(onPressed: (){
+
+              if( formKey.currentState!.validate()){
+
+                  setState(() {
+                    result = num1+num2;
+                  });
+
+              }
+
+            }, child: const Text('Add')),
+            ElevatedButton(onPressed: (){
+              formKey.currentState!.reset();
+
+              setState(() {
+                result = 0;
+              });
+            }, child: const Text('Clear')),
+
+            Text(result.toString()),
+          ],),
+        ),
+      ),
     );
   }
 }
